31a2375af83e019e101179f1d160c818577b3ad3
yossu-y <yangyuub@gmail.com>
2022-05-31 12:37:16 +0000

    [update]homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   
-  namespace :user do
-    get 'homes/top'
-    get 'homes/about'
-  end
   # 利用者用ルーティング
+  scope module: :user do
+    root to: "homes#top"
+    get "about" => "homes#about", as: "about"
+    
+  end
+  
   devise_for :users,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
+      registrations: "public/registrations",
+      sessions: 'public/sessions'
+    }
   
   
   # 管理者用ルーティング
